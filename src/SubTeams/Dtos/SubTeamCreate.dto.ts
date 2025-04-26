41da59937d038fbc03b80ec6c8fe8324b61a5247
thewitcher7667 <aymanwilliam604@gmail.com>
2025-04-26 19:43:10 +0300

    Remove regex in the name of comm team sub teams

--- src/SubTeams/Dtos/SubTeamCreate.dto.ts
+++ src/SubTeams/Dtos/SubTeamCreate.dto.ts
@@ -7,17 +7,12 @@ export class SubTeamCreateDto
     @IsString()
     @MaxLength(15,{message:"Name maximum length is 15"})
     @IsNotEmpty({message:"Name must be not empty"})
-    @Matches(/^[a-zA-Z0-9]+$/, {
-        message: 'Name must contain only letters and numbers (no spaces or special characters).',
-    })
     @ApiProperty({
         name:"Name",
         maxLength:15,
         type:"string",
         nullable:false,
         required:true,
-        description:"Regex: /^[a-zA-Z0-9]+$/",
-        pattern:"/^[a-zA-Z0-9]+$/"
     })
     Name:string;
 
